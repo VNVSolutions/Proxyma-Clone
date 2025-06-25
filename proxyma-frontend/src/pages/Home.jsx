b5654dc0e09c7eb96d46a80a1318f9371a519d03
Alex Kozak <56194044+Alex-byte256@users.noreply.github.com>
2025-06-25 21:09:43 +0300

    add hero markup

--- proxyma-frontend/src/pages/Home.jsx
+++ proxyma-frontend/src/pages/Home.jsx
@@ -1,19 +1,14 @@
 import Container from "../components/utils/Container/Container.jsx";
 import { useTranslation } from 'react-i18next';
-import {useEffect} from "react";
+import Hero from "../components/hero/Hero.jsx";
 
 
 function Home() {
     const { t, i18n } = useTranslation();
 
-    useEffect(()=>{
-        i18n.changeLanguage('uk');
-    },[])
-    return<>
-        <Container>
-            {t('welcome')}
-        </Container>
-    </>
+    return<main>
+        <Hero/>
+    </main>
 }
 
 export default Home;
